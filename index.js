c5f4236e38229450765dabb077ac05bd7cbb7397
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-04-09 17:30:57 -0300

    [INFRA] Add mongoose and connection with mongodb

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const createServer = require('./src/infra/webserver/server');
 require('dotenv').config();
+const database = require('./src/infra/config/database');
 
 const start = () => {
   try {
     createServer();
+    database.connect();
     console.log('server started');
   } catch (err) {
     console.error(err);
