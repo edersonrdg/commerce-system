024433ac2bc086312fe2766e8e3b539bc8964991
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-04-09 16:54:34 -0300

    [INFRA] create webserver with express and use dotenv

---
+++ index.js
@@ -0,0 +1,15 @@
+const createServer = require('./src/infra/webserver/server')
+require('dotenv').config();
+
+const start = () => {
+    try {
+        createServer()
+        console.log('server started')
+    }
+    catch (err) {
+        console.error(err)
+        process.exit(1)
+    }
+}
+
+start()
---
+++ src/infra/webserver/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const createServer = () => {
+
+    const server = express()
+
+    server.listen(process.env.PORT || 3000)
+}
+
+module.exports = createServer
